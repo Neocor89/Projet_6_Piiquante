09709f09769ef57ee114722750eae54f38062596
Neocor89 <ncor89b@gmail.com>
2022-04-28 14:49:53 +0200

    install dependencies and add js files app and server

---
+++ back/server.js
@@ -0,0 +1,41 @@
+const http = require('http'); // Importation du package http
+const app = require('./app'); // On importe app pour utilisation de l'application sur le serveur
+
+// Configuration du port de connexion
+const normalizePort = (val) => {
+  // Renvoie un port valide avec la fonction normalizePort
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+// Si aucun port n'est fourni on écoutera sur le port 3000
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// la fonction errorHandler capte les différentes erreurs et l' enregistre dans le serveur
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
